71a5e946b1cdef8225b2f4df81b7e6abc2709370
Daniel Schutko <danielschutko@gmail.com>
2020-06-12 13:29:09 +0200

    [added] empty project + time cubes

---
+++ ChaosSymmetry_Project/Assets/TimeManager.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public float slowdownFactor = 0.05f;
+    public float slowdownLenght = 2f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoSlowmotion();
+        }
+
+        Time.timeScale += (1f / slowdownLenght) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+
+    public void DoSlowmotion()
+    {
+        Time.timeScale = slowdownFactor;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+}
