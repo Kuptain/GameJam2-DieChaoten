ab7e556403945ccb56361a83310e0e1ba363307d
Daria <eleonora.pankau@gmail.com>
2020-06-12 17:06:05 +0200

    cluster scripts

---
+++ ChaosSymmetry_Project/Assets/Scripts/Procedural Generation/ClusterGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ClusterGenerator 
+{
+    public static Cluster Generate()
+    {
+        return new Cluster(4, 4,
+            new Wing[] {
+                new Wing(
+                    new RectInt(0,0,4,4),
+                    new Story[]
+                    {
+                        new Story(0, new Wall[((4+4)*2)])
+                    },
+                    new Roof()
+                    )
+            });
+    }
+}
