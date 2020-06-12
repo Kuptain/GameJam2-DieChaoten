4b957fd34d1732e1801ddd373e7fd1872bbecf64
Bianca <bianca@trizero.de>
2020-06-12 16:02:58 +0200

    [script] FastForward

--- ChaosSymmetry_Project/Assets/Scripts/TimeManager.cs
+++ ChaosSymmetry_Project/Assets/Scripts/TimeManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 public class TimeManager : MonoBehaviour
 {
     public float slowdownFactor = 0.05f;
-    public float slowdownLenght = 2f;
+    public float slowdownLenght = 4f;
+
+    public float fastFactor = 2f;
+    public float fastLength = 4f;
 
     void Update()
     {
@@ -15,11 +18,21 @@ public class TimeManager : MonoBehaviour
 
         Time.timeScale += (1f / slowdownLenght) * Time.unscaledDeltaTime;
         Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            DoFastforward();
+        }
     }
 
     public void DoSlowmotion()
     {
         Time.timeScale = slowdownFactor;
         Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    public void DoFastforward()
+    {
+        Time.timeScale = fastFactor;
     }
 }
