295c05933245b804715a17e1f3c1b55df3d66964
Daria <eleonora.pankau@gmail.com>
2020-06-18 13:57:37 +0200

    [added] PowerUp system
    mit longerfloat, longerfreeze, higherjump powerups

---
+++ ChaosSymmetry_Project/Assets/Scripts/RandomPowerUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class RandomPowerUp : MonoBehaviour
+{
+    public string thisPowerUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisPowerUp = PowerUpManager.instance.powerUps[Random.Range(0, PowerUpManager.instance.powerUps.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            PowerUpManager.instance.currentPowerUp = thisPowerUp;
+            UIManager.instance.currentPowerup.GetComponent<Text>().text = thisPowerUp;
+            if(thisPowerUp == "longerFreeze")
+            {
+                UIManager.instance.currentFreezeTime = PowerUpManager.instance.longerFreezeData;
+                UIManager.instance.freezetimer = PowerUpManager.instance.longerFreezeData;
+            }
+            else
+            {
+                UIManager.instance.currentFreezeTime = 5;
+                UIManager.instance.freezetimer = 5;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
