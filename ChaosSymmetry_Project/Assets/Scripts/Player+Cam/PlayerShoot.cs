124bbdd0ebe68bd1fd8e01bf91de1dfdd1ff318c
Daria <eleonora.pankau@gmail.com>
2020-06-24 19:57:58 +0200

    [fixed] pausenmenu

--- ChaosSymmetry_Project/Assets/Scripts/Player+Cam/PlayerShoot.cs
+++ ChaosSymmetry_Project/Assets/Scripts/Player+Cam/PlayerShoot.cs
@@ -15,7 +15,6 @@ public class PlayerShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
         cubeManager = CubeManager.instance;
 
     }
