f63d5cd737a2f4b188fd619ec4f02b5da58f589a
Daria <eleonora.pankau@gmail.com>
2020-06-26 17:41:47 +0200

    [added] rubble

---
+++ ChaosSymmetry_Project/Assets/Scripts/Rubble/RotateRubble.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRubble : MonoBehaviour
+{
+    CubeManager cm;
+    float maxRotation;
+    Vector3 randomRotate;
+    public bool exploding;
+    GameObject platform;
+    Vector3 startPosition;
+    CubeDestroy platformScript;
+    Rigidbody rigid;
+    float sendBackManual;
+    Quaternion startRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm = CubeManager.instance;
+        maxRotation = cm.orbitMaxRotation;
+        startPosition = transform.position;
+        if (transform.parent.GetComponent<RubbleExplosion>() != null)
+        {
+            startRotation = transform.rotation;
+            sendBackManual = cm.sendBackManual;
+            platform = gameObject.transform.parent.transform.parent.GetChild(0).GetChild(0).gameObject;
+            platformScript = platform.GetComponent<CubeDestroy>();
+            rigid = GetComponent<Rigidbody>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent.GetComponent<RubbleExplosion>() != null)
+        {
+            if (exploding)
+            {
+                transform.Rotate(randomRotate.x * Time.deltaTime * cm.currentSlowmo,
+                            randomRotate.y * Time.deltaTime * cm.currentSlowmo,
+                            randomRotate.z * Time.deltaTime * cm.currentSlowmo);
+            }
+
+            if (platformScript.sendingBack == true)
+            {
+                SendBack();
+            }
+        }
+    }
+
+    public void RandomizeRotation()
+    {
+        randomRotate = new Vector3(Random.Range(-maxRotation, maxRotation), Random.Range(-maxRotation, maxRotation), Random.Range(-maxRotation, maxRotation));
+
+    }
+
+    public void SendBack()
+    {
+        if (platformScript.freezeThisCluster == false && platformScript.bubbleFreeze == false)
+        {
+            //pushMode = 0;
+            transform.position = Vector3.Slerp(transform.position, startPosition, sendBackManual * cm.currentSlowmo);
+            transform.rotation = Quaternion.Lerp(transform.rotation, startRotation, sendBackManual * cm.currentSlowmo);
+
+            rigid.constraints = RigidbodyConstraints.FreezeRotation;
+            rigid.constraints = RigidbodyConstraints.FreezePosition;
+        }
+        else if (platformScript.freezeThisCluster == true || platformScript.bubbleFreeze == true)
+        {
+            rigid.constraints = RigidbodyConstraints.FreezeRotation;
+            rigid.constraints = RigidbodyConstraints.FreezePosition;
+        }
+    }
+}
---
+++ ChaosSymmetry_Project/Assets/Scripts/Rubble/RubbleExplosion.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubbleExplosion : MonoBehaviour
+{
+    public float explosionForce;
+    public LayerMask explosionMask;
+    public Collider[] hitColliders;
+    public float blastradius = 3;
+    CubeManager cm;
+    Vector3 randomRotate;
+    float maxRotation;
+    bool exploded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cm = CubeManager.instance.GetComponent<CubeManager>();
+        hitColliders = Physics.OverlapSphere(this.transform.position, blastradius, explosionMask);
+        maxRotation = CubeManager.instance.orbitMaxRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (exploded)
+        {
+            foreach (Transform child in transform)
+            {
+                child.GetComponent<RotateRubble>().RandomizeRotation();
+                child.GetComponent<RotateRubble>().exploding = true;
+            }
+            exploded = false;
+        }
+    }
+
+    public void Explode()
+    {
+        if (exploded == false)
+        {
+
+            foreach (Collider hitCol in hitColliders)
+            {
+                if (hitCol.GetComponent<Rigidbody>() != null)
+                {
+                    hitCol.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, this.transform.position, blastradius, 1, ForceMode.Impulse);
+                }
+            }
+            //RandomizeRotation();
+            exploded = true;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        //hitColliders = Physics.OverlapSphere(this.transform.position, blastradius, explosionMask);
+        Gizmos.DrawWireSphere(transform.position, blastradius);
+    }
+}
