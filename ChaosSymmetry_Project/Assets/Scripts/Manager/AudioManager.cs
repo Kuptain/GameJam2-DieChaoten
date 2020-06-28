d1461d04f53273c1997a3ac17aeeb37ddfdff918
Daria <eleonora.pankau@gmail.com>
2020-06-28 15:40:19 +0200

    [added] Audiomanager

---
+++ ChaosSymmetry_Project/Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioClip slomoOn;
+    public AudioClip slomoOff;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
